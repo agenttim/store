b6547a3f1a4fb14c449f69edf99a53b5bd1d989e
agent_tim <agent_tim@mail.ru>
2024-02-12 16:55:25 +0500

    Блок 5 в процессе

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from products.models import Product, ProductCategory, Basket
 
 
@@ -10,6 +10,7 @@ def index(request):
     context = {'title': 'Store'}
     return render(request, 'products/index.html', context)
 
+
 def products(request):
     context = {'title': 'Store - Каталог',
                'products': Product.objects.all(),
@@ -17,6 +18,8 @@ def products(request):
                }
     return render(request, 'products/products.html', context)
 
+
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -30,6 +33,8 @@ def basket_add(request, product_id):
 
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
+
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.contrib import auth, messages
 from django.shortcuts import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from products.models import Basket
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -36,6 +37,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
